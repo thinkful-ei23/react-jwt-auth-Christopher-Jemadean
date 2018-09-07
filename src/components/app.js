f437d23d24f88efbe669753a194be3fb7a557f43
Jemadean Dobrosielski <jemadean@gmail.com>
2018-09-07 14:30:01 -0700

    all but warning dialog complete

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {Route, withRouter} from 'react-router-dom';
+import IdleTimer from 'react-idle-timer'
 
 import HeaderBar from './header-bar';
 import LandingPage from './landing-page';
 import Dashboard from './dashboard';
 import RegistrationPage from './registration-page';
-import {refreshAuthToken} from '../actions/auth';
+import {refreshAuthToken, clearAuth} from '../actions/auth';
+import {clearAuthToken} from '../local-storage';
 
 export class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.idleTimer = null
+        this.onActive = this._onActive.bind(this)
+        this.onIdle = this._onIdle.bind(this)
+      }
+
     componentDidUpdate(prevProps) {
         if (!prevProps.loggedIn && this.props.loggedIn) {
             // When we are logged in, refresh the auth token periodically
@@ -26,7 +35,7 @@ export class App extends React.Component {
     startPeriodicRefresh() {
         this.refreshInterval = setInterval(
             () => this.props.dispatch(refreshAuthToken()),
-            60 * 60 * 1000 // One hour
+            10 * 60 * 1000 // 10 min
         );
     }
 
@@ -38,9 +47,24 @@ export class App extends React.Component {
         clearInterval(this.refreshInterval);
     }
 
+    logOut() {
+        this.props.dispatch(clearAuth());
+        clearAuthToken();
+    }
+
     render() {
         return (
             <div className="app">
+                <IdleTimer
+                    ref={ref => { this.idleTimer = ref }}
+                    element={document}
+                    onActive={this.onActive}
+                    onIdle={this.onIdle}
+                    timeout={1000 * 15}>
+            
+                    <h1>Child Components</h1>
+            
+                </IdleTimer>
                 <HeaderBar />
                 <Route exact path="/" component={LandingPage} />
                 <Route exact path="/dashboard" component={Dashboard} />
@@ -48,6 +72,17 @@ export class App extends React.Component {
             </div>
         );
     }
+
+    _onActive(e) {
+        console.log('user is active', e)
+        console.log('time remaining', this.idleTimer.getRemainingTime())
+      }
+     
+      _onIdle(e) {
+        console.log('user is idle', e);
+        console.log('last active', this.idleTimer.getLastActiveTime());
+        this.logOut();
+      }
 }
 
 const mapStateToProps = state => ({
